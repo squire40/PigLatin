d33072d65f637d135479cac08999511b92baf155
Dave Bourke <squire40@hotmail.com>
2018-10-15 20:48:21 -0400

    Initial Commit

---
+++ PigLatin/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace PigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = null;
+            do
+            {
+                
+            } while (answer.ToLower().Equals("y"));
+
+        }
+    }
+}
